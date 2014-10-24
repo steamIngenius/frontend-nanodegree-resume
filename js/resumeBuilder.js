0e86d8d4312327099d173f1825f1ae96700a60c3
Micah Hawkins <micahbhawkins@gmail.com>
2014-10-23 20:18:45 -0700

    Add name and role to page via jQuery selector

---
+++ js/resumeBuilder.js
@@ -0,0 +1,4 @@
+var formattedName = HTMLheaderName.replace("%data%", "Micah Hawkins");
+var formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
+
+$("#main").prepend(formattedName + formattedRole);
