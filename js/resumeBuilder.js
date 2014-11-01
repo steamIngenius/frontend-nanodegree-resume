d058768886a8d43ec30da46b760ec69ee65ed0e6
Micah Hawkins <micahbhawkins@gmail.com>
2014-11-01 10:49:17 -0700

    Encapsulate and flesh-out resume objects (started) 	currently on the bio object

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -14,7 +14,27 @@ var work = {
 			"dates" : "Something - Dec 7th 2012",
 			"description" : "Support, Training, Technology Systems Implementation"
 	    }
-	]
+	],
+	display: function() {
+		// if there are jobs to be added
+		if ( work.jobs.length > 0 ) {
+			// add dem jobs to workExperience
+			for ( job in work.jobs ) {
+				// add HTMLworkStart to workExperience for each job
+				// format each employer with HTMLworkEmployer and each job title with HTMLjobTitle
+				// use the :last selector to add the info to the correct .work-entry element (the last one appended)
+				var formattedWorkEmployer = HTMLworkEmployer.replace('%data%', work.jobs[job].employer);
+				var formattedWorkTitle = HTMLworkTitle.replace('%data%', work.jobs[job].title);
+				var formattedWorkLocation = HTMLworkLocation.replace('%data%', work.jobs[job].location);
+				var formattedWorkDates = HTMLworkDates.replace('%data%', work.jobs[job].dates);
+				var formattedWorkDescription = HTMLworkDescription.replace('%data%', work.jobs[job].description);
+
+				$("#workExperience").append(HTMLworkStart);
+				$('.work-entry:last').append(formattedWorkEmployer + formattedWorkTitle + formattedWorkLocation
+					+ formattedWorkDates + formattedWorkDescription);
+			}
+		}
+	} // end of display function
 };
 
 var projectData = {
@@ -49,10 +69,48 @@ var projectData = {
 var bio = {
 	"name" : "Micah Hawkins",
 	"role" : "Web Developer",
-	"contact" : "micahbhawkins@gmail.com",
-	"pictureURL" : "",
+	"contacts" : { "email" : "micahbhawkins@gmail.com",
+			"mobile" : "",
+			"github" : "",
+			"twitter" : ""
+		},
+	"pictureURL" : "http://www.fillmurray.com/200/200",
 	"welcomeMessage" : "Greetings!",
-	"skills" : [ "Troubleshooting", "WebDev", "Whatever OS", "Goobling Stuff", "RTFM" ]
+	"skills" : [ "Troubleshooting", "WebDev", "Whatever OS", "Goobling Stuff", "RTFM" ],
+
+	// function to internationalize our name - moved to the bio object for encapsulation
+	inName: function() {
+		name = bio.name;
+		spaceIndex = name.indexOf(' ');
+		var interName = name[0].toUpperCase() + name.slice(1, spaceIndex).toLowerCase() + name.slice(spaceIndex).toUpperCase();
+		return interName;
+	},
+
+	// display function for our bio object
+	display: function() {
+		var formattedName = HTMLheaderName.replace("%data%", bio.inName(bio.name));
+		var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+
+		// this adds my name/role to the beginnging of the page, before any other sections
+		$("#main").prepend(formattedName + formattedRole);
+
+		// the rest of our bio information will go into #header
+		$("#header").append(HTMLWelcomeMsg.replace('%data%', bio.welcomeMessage));
+		$("#header").append(HTMLemail.replace('%data%', bio.contact));
+		$("#header").append(HTMLbioPic.replace('%data%', bio.pictureURL));
+		// $("#main").append(bio.skills);
+		// if there are skills to be added
+		if ( bio.skills.length > 0 ) {
+			// set up the page to receive skills
+			$("#header").append(HTMLskillsStart);
+
+			// add skills
+			for ( i in bio.skills ) {
+				var formattedSkill = HTMLskills.replace('%data%', bio.skills[i]);
+				$("#skills").append(formattedSkill);
+			}
+		}
+	} // end of display function
 };
 
 var education = {
@@ -68,61 +126,14 @@ var education = {
 	]
 };
 
-function inName() {
-	name = bio.name;
-	spaceIndex = name.indexOf(' ');
-	var interName = name[0].toUpperCase() + name.slice(1, spaceIndex).toLowerCase() + name.slice(spaceIndex).toUpperCase();
-	return interName;
-}
 
-var formattedName = HTMLheaderName.replace("%data%", inName(bio.name));
-var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-
-$("#main").prepend(formattedName + formattedRole);
 // $("#main").append(internationalizeButton);
 
-/*
-$("#main").append(bio.name);
-$("#main").append(bio.role);
-$("#main").append(bio.contact);
-$("#main").append(bio.pictureURL);
-$("#main").append(bio.welcomeMessage);
-$("#main").append(bio.skills);
-*/
 
-// if there are skills to be added
-if ( bio.skills.length > 0 ) {
-	// set up the page to receive skills
-	$("#header").append(HTMLskillsStart);
-
-	// add skills
-	for ( i in bio.skills ) {
-		var formattedSkill = HTMLskills.replace('%data%', bio.skills[i]);
-		$("#skills").append(formattedSkill);
-	}
-}
-
-var displayWork = function() {
-	// if there are jobs to be added
-	if ( work.jobs.length > 0 ) {
-		// add dem jobs to workExperience
-		for ( job in work.jobs ) {
-			// add HTMLworkStart to workExperience for each job
-			// format each employer with HTMLworkEmployer and each job title with HTMLjobTitle
-			// use the :last selector to add the info to the correct .work-entry element (the last one appended)
-			var formattedWorkEmployer = HTMLworkEmployer.replace('%data%', work.jobs[job].employer);
-			var formattedWorkTitle = HTMLworkTitle.replace('%data%', work.jobs[job].title);
-			var formattedWorkLocation = HTMLworkLocation.replace('%data%', work.jobs[job].location);
-			var formattedWorkDates = HTMLworkDates.replace('%data%', work.jobs[job].dates);
-			var formattedWorkDescription = HTMLworkDescription.replace('%data%', work.jobs[job].description);
-
-			$("#workExperience").append(HTMLworkStart);
-			$('.work-entry:last').append(formattedWorkEmployer + formattedWorkTitle + formattedWorkLocation
-				+ formattedWorkDates + formattedWorkDescription);
-		}
-	}
-}
-// 
-displayWork();
+// display resume information
+bio.display();
+work.display();
 projectData.display();
+
+// add our map!
 $('#mapDiv').append(googleMap);
